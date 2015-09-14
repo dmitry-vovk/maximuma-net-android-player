e4af9eb9690105e9a202d5704357246003cb7a2d
Dmytro Vovk <dmitry.vovk@gmail.com>
2015-09-14 19:59:53 +0300

    Initial

---
+++ app/src/main/java/net/maximuma/player/maximumplayer/PlayerActivity.java
@@ -0,0 +1,74 @@
+package net.maximuma.player.maximumplayer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    protected static final String ADDRESS = "http://android-tv.maximuma.net";
+
+    protected WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        load();
+    }
+
+    protected void load() {
+        JavaScriptInterface JSInterface;
+        setContentView(R.layout.activity_player);
+        myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl(ADDRESS);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        // register class containing methods to be exposed to JavaScript
+        JSInterface = new JavaScriptInterface(this);
+        myWebView.addJavascriptInterface(JSInterface, "JSInterface");
+    }
+
+    @Override
+    protected void onPause() {
+        myWebView.destroy();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        load();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        load();
+    }
+
+    @Override
+    protected void onStop() {
+        myWebView.destroy();
+        super.onStop();
+    }
+
+    public class JavaScriptInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        JavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void changeActivity() {
+            Intent i = new Intent(PlayerActivity.this, PlayerActivity.class);
+            startActivity(i);
+            finish();
+        }
+    }
+}
